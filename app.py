c2a0b06755a0754760d2d0df9ce3645af1b0c397
Sumit Sarwade <sum15it99@gmail.com>
2022-08-31 10:35:31 +0530

    heruku test2

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import pandas as pd
 import sklearn
 import numpy as np
 
-st.title("Diabeties Detetcion xx")
+st.title("Diabeties Detetcion")
 
-Pregnancies = st.number_input('Pregnancies', 0, 1220000, 10)
-Glucose = st.number_input('Glucose', 0, 1220000, 10)
-BloodPressure = st.number_input('BloodPressure', 0, 1220000, 10)
-SkinThickness = st.number_input('SkinThickness', 0, 1220000, 10)
-Insulin = st.number_input('Insulin', 0, 1220000, 10)
-BMI = st.number_input('BMI', 0, 1220000, 10)
-DiabetesPedigreeFunction = st.number_input('DiabetesPedigreeFunction', 0, 1220000, 10)
-Age = st.number_input('Age', 0, 1220000, 10)
+Pregnancies = st.number_input('Pregnancies', 0, 17, 3)
+Glucose = st.number_input('Glucose', 0, 200, 120)
+BloodPressure = st.number_input('BloodPressure', 0, 123, 69)
+SkinThickness = st.number_input('SkinThickness', 0, 100, 20)
+Insulin = st.number_input('Insulin', 0, 847, 79)
+BMI = st.number_input('BMI', 0, 68 , 31)
+DiabetesPedigreeFunction = st.number_input('DiabetesPedigreeFunction', 0, 3 , 1)
+Age = st.number_input('Age', 21, 99, 33)
 
 dict_ = {
             "Pregnancies": [Pregnancies],
